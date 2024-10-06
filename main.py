3d1d43f3925b0772945d81996faaf55dc59f7eab
Mohammed Zaid <70617604+zaid45645@users.noreply.github.com>
2024-10-06 13:39:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 
-
 sun_radius = 60
 earth_radius = 30
 venus_radius = 30
@@ -17,15 +16,24 @@ uranus_radius = 40
 sun_color = 'yellow'
 earth_color = 'blue'
 mercury_color = 'grey'
-venus_color = 'brown'
+venus_color = 'orange'
 mars_color = 'red'
-jupitur_color = 'brown'
-saturn_color = 'brown'
-neptune_color = 'blue'
-uranus_color = 'blue'
+jupitur_color = '#c99039'
+saturn_color = '#ceb8b8'
+neptune_color = '#7CB7BB'
+uranus_color = '#d9ddf4'
 
 orbit_color = 'grey'
 
+lucy_radius = 5
+vesta_radius = 4
+eros_radius = 3
+didymos_radius = 5
+
+lucy_color = 'green'
+vesta_color = 'brown'
+eros_color = 'red'
+didymos_color = 'grey'
 
 class Planet():
     def __init__(self, name, distance, speed, radius, color):
@@ -58,6 +66,10 @@ class Orrery:
             Planet('Saturn', 200, 0.0005, saturn_radius, saturn_color),
             Planet('Uranus',230, 0.0002, uranus_radius, uranus_color),
             Planet('Neptune',260, 0.0001, neptune_radius, neptune_color),
+            Planet('Lucy', 120, 0.004, lucy_radius, lucy_color),  # new planet
+            Planet('Vesta', 160, 0.0035, vesta_radius, vesta_color),  # new planet
+            Planet('Eros', 140, 0.003, eros_radius, eros_color), 
+            Planet('Didymos', 125, 0.00026, didymos_radius, didymos_color), # new planet
         ]
 
     def update(self, frame):
@@ -70,13 +82,17 @@ class Orrery:
             planet.update()
             x, y, z = planet.get_position()
             self.ax.scatter(x, y, z, c=planet.color, s=planet.radius*10)
-            self.ax.text(x, y, z, planet.name, ha='center', va='center', size=10)
-            theta = np.linspace(0, 2*np.pi, 100)
-            x_orbit = planet.distance * np.cos(theta)
-            y_orbit = planet.distance * np.sin(theta)
-            z_orbit = np.zeros_like(theta)
-            self.ax.plot(x_orbit, y_orbit, z_orbit, c=orbit_color, lw=1)
+            self.ax.text(x, y, z, planet.name, ha='left', va='bottom', size=10)
+            if planet.name not in ['Lucy', 'Vesta', 'Eros', 'Didymos']:
+                theta = np.linspace(0, 2*np.pi, 100)
+                x_orbit = planet.distance * np.cos(theta)
+                y_orbit = planet.distance * np.sin(theta)
+                z_orbit = np.zeros_like(theta)
+                self.ax.plot(x_orbit, y_orbit, z_orbit, c=orbit_color, lw=1)
+
         self.ax.set_title(' Space Orrery - DevFreaks')
+
+        
 
     def animate(self):
         ani = animation.FuncAnimation(self.fig, self.update, frames=200, interval=50)
